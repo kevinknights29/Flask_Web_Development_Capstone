878d5003bed68be6c0d72bbe9d348c30dcb105e3
Kevin Knights <kevinknights29@hotmail.com>
2023-11-23 00:45:58 -0500

    WIP: rendering templates

---
+++ main.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+
+from flask import Flask
+from flask import render_template
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/user/<name>")
+def user(name):
+    return render_template("user.html", name=name)
